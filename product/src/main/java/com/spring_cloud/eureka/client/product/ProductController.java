bd3ceb189ee6de05894fd5d4a8092bc73d7e0ee4
vpfl95 <k_dh610@naver.com>
2025-02-07 14:09:47 +0900

    feat: Config Server

--- product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
+++ product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
@@ -2,20 +2,25 @@ package com.spring_cloud.eureka.client.product;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ProductController {
 
     @Value("${server.port}")
     private String serverPort;
 
+    @Value("${message}")
+    private String message;
+
     @GetMapping("/product")
     public String getProduct() {
 
-        return "Product "  + "info!!! From port : " + serverPort;
+        return "Product "  + "info!!! From port : " + serverPort + "and message : " + message;
     }
 
 }
